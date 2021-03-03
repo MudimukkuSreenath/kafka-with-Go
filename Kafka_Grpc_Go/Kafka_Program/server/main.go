2394707eb6749b7d31765c6890aff8c098c72d07
MudimukkuSreenath <72983395+MudimukkuSreenath@users.noreply.github.com>
2021-03-03 13:05:24 +0530

    Add files via upload

---
+++ Kafka_Grpc_Go/Kafka_Program/server/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/MudimukkuSreenath/kaffka_with_Go/proto"
+
+	"github.com/segmentio/kafka-go"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct{}
+
+const (
+	topic         = "TestTopic"
+	brokerAddress = "localhost:9092"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+	srv := grpc.NewServer()
+	proto.RegisterProducerServiceServer(srv, &server{})
+	reflection.Register(srv)
+
+	if e := srv.Serve(listener); e != nil {
+		panic(err)
+	}
+}
+
+func (s *server) Producer(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a := request.GetUsername()
+
+	result := a
+
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{brokerAddress},
+		Topic:   topic,
+	})
+
+	err := w.WriteMessages(ctx, kafka.Message{
+		Key:   []byte(result),
+		Value: []byte(result),
+	})
+
+	if err != nil {
+		panic("could not write message " + err.Error())
+	}
+
+	fmt.Println("writes:", result)
+	time.Sleep(time.Second)
+
+	return &proto.Response{Result: "success"}, nil
+}
